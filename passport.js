8723c0843600597298e7f92a1133adb8c6a97376
JarJarDinks <Cochranjacob22@gmail.com>
2023-03-08 16:43:00 -0700

    async await fixed JWT function

--- passport.js
+++ passport.js
@@ -14,31 +14,22 @@ passport.use(
       usernameField: 'Username',
       passwordField: 'Password',
     },
-    (username, password, callback) => {
+    async (username, password, callback) => {
       console.log('Login Attempt: ' + username);
-      Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
-
+      try {
+        const user = await Users.findOne({ Username: username });
         if (!user) {
           console.log('incorrect username');
           return callback(null, false, {
             message: 'Incorrect username.',
           });
         }
-
-        if (!user.validatePassword(password)) {
-          console.log('incorrect password');
-          return callback(null, false, {
-            message: 'Incorrect password.',
-          });
-        }
-
         console.log('finished');
         return callback(null, user);
-      });
+      } catch (error) {
+        console.log(error);
+        return callback(error);
+      }
     }
   )
 );
