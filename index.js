676b8939674659fd47458555951f8157d6c8cef6
JarJarDinks <Cochranejacob22@gmail.com>
2023-04-27 01:32:00 -0700

    cors test

--- index.js
+++ index.js
@@ -39,27 +39,7 @@ databaseConnect();
 
 // Use cors to allow cross-origin requests
 const cors = require('cors');
-const allowedOrigins = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://mymovieapidb.herokuapp.com',
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 
 const auth = require('./auth')(app);
 const passport = require('passport');
